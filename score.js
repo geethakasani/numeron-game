e3c294abb1d1312cb4f65cac0a4a60e3fc132448
geethakasani <160598661+geethakasani@users.noreply.github.com>
2024-03-18 23:15:31 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+function displayScore() {
+    const scoreBoard = document.getElementById("score-board");
+    const playerScore = localStorage.getItem("playerScore");
+    scoreBoard.textContent = playerScore ? playerScore : 0;
+}
+displayScore();
+const playAgainButton = document.getElementById("play-again-button");
+playAgainButton.addEventListener("click", function () {
+    window.location.href = "game.html"; 
+});
